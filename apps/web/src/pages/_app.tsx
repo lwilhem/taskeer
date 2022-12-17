2149ca06286fb208f8497f263eb9c4aaeed78bb7
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-12-17 21:28:11 +0100

    Tailwind & suapabse init

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, type Session } from '@supabase/auth-helpers-react'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
 
-export default function TaskeerWebapp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function TaskeerWebapp({
+  Component,
+  pageProps
+}: AppProps<{ initialSession: Session }>) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
 }
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <section>
-      <h1>Hello World !</h1>
+      <h1 className="text-3xl font-bold underline">Hello World !</h1>
     </section>
   )
 }
